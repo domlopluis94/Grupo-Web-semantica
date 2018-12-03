bf9ccdb3d5b578388e9c5cae54537bae176463d6
Luis Domínguez López <domlopluis@gmail.com>
2018-12-03 23:10:17 +0100

    handson 4,5 e primera etapa APP

---
+++ aplicacion/JavaJAr/TenerifeGym/src/main/java/upm/oeg/wsld/jena/FuncionesAndroid.java
@@ -0,0 +1,56 @@
+package upm.oeg.wsld.jena;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.util.FileManager;
+import org.apache.jena.vocabulary.VCARD;
+
+/**
+ * 
+ * 
+ */
+public class FuncionesAndroid
+{
+
+	/**
+	 * ArrayList con el nombre de las maquinas sin repetir nombres 
+	 * @return
+	 */
+	public ArrayList<String> listademaquinas (){
+		ArrayList<String> ganso = null;
+		
+		return ganso;
+	}
+	
+	
+	/**
+	 * Le pasamos el Nombre de la maquina y nos retorna todas las latitudes y longitudes de las maquinas con ese nombre 
+	 * @param Nmaquina
+	 * @return
+	 */
+	public int [] cordenadasMaquinas (String Nmaquina){
+		int [] coordenadas = null;
+		return coordenadas;
+	}
+	
+	
+	/**
+	 * 
+	 * @param Nmaquina
+	 * @return
+	 */
+	public String UriMaquina (String Nmaquina){
+
+		return "";
+	}
+	
+	
+	//para pruebas
+	public static void main(String args[])
+	{
+		
+	}
+	
+}
