7a3ba639e41ea28dca550b037593b5884ab870c5
Luis Domínguez López <domlopluis@gmail.com>
2018-12-04 22:38:57 +0100

    auxilio

--- aplicacion/JavaJAr/TenerifeGym/src/main/java/upm/oeg/wsld/jena/FuncionesAndroid.java
+++ aplicacion/JavaJAr/TenerifeGym/src/main/java/upm/oeg/wsld/jena/FuncionesAndroid.java
@@ -3,8 +3,20 @@ package upm.oeg.wsld.jena;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.ontology.OntProperty;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.util.FileManager;
+import org.apache.jena.util.iterator.ExtendedIterator;
 import org.apache.jena.vocabulary.VCARD;
 
 /**
@@ -13,7 +25,7 @@ import org.apache.jena.vocabulary.VCARD;
  */
 public class FuncionesAndroid
 {
-
+	
 	/**
 	 * ArrayList con el nombre de las maquinas (modelo) sin repetir nombres 
 	 * @return
@@ -58,7 +70,48 @@ public class FuncionesAndroid
 	//para pruebas
 	public static void main(String args[])
 	{
+		String filename = "bin/resources/bio_saludables_final.rdf";
+		String ns = "https://www.santacruzdetenerife.es/";
+		// Create an empty model
+		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM);
 		
+		
+		// Use the FileManager to find the input file
+		InputStream in = FileManager.get().open(filename);
+
+		if (in == null)
+			throw new IllegalArgumentException("File: "+filename+" not found");
+		
+		model.read(in,null);
+		
+		OntClass maquina = model.getOntClass(ns+"Clases/MaquinaPublica");
+		ExtendedIterator ite = maquina.listInstances();
+		
+		Individual instance = null;
+		Property propiedad = model.getOntProperty(ns+"Propiedades/HasModel");
+		while(ite.hasNext()) 
+		{
+			instance = (Individual) ite.next();
+			System.out.println("LocalName: " + instance.getPropertyValue(propiedad) + " " + instance.getURI());
+		}
+		
+		/*
+		String PREFIX = "PREFIX clases: <https://www.santacruzdetenerife.es/Clases/> \n"
+				+ "PREFIX propiedades: <https://www.santacruzdetenerife.es/Propiedades/> \n"
+				+ "PREFIX ns:<http://www.santacruzdetenerife.es/>";
+		
+		String queryString2 = PREFIX + " SELECT ?x WHERE {?x a clases:MaquinaPublica} ";
+		Query query = QueryFactory.create(queryString2);
+		QueryExecution qexec = QueryExecutionFactory.create(query, model) ;
+		ResultSet results = qexec.execSelect() ;
+		while (results.hasNext())
+		{
+			QuerySolution binding = results.nextSolution();
+			Resource subj = (Resource) binding.get("x");
+			Literal fn = binding.getLiteral("x");
+			System.out.println("Subject: " + subj.getURI() + " " + fn);
+		}
+		*/
 	}
 	
 }
